5d60c6c7cdc702b359aac59f483e3597d5560f83
JulianCedric <julian.pormentilla@gmail.com>
2020-05-13 14:35:43 -0400

    see julian_notes.rb for update info

--- config/environment.rb
+++ config/environment.rb
@@ -7,3 +7,15 @@ require_relative '../lib/screen.rb'
 require_relative '../lib/award.rb'
 require_relative '../lib/director.rb'
 require_relative '../lib/movie_contract.rb'
+
+require 'bundler/setup'
+Bundler.require
+
+ActiveRecord::Base.establish_connection(
+  adapter: 'sqlite3',
+  database: "db/development.sqlite3"
+)
+
+ActiveRecord::Base.logger = Logger.new(STDOUT)
+
+require_all 'app'
---
+++ julian_notes.rb
@@ -0,0 +1,14 @@
+# Changes I'm making to to convert this project from OOB Ruby to Active Record:
+
+# config
+    # database.yml (created file)
+    # environment.rb (added to)
+
+# db (created folder)
+    # seeds.rb (created file)
+
+# Gemfile (created file)
+
+# Gemfile.lock (created file)
+
+# Rakefile (created file)
